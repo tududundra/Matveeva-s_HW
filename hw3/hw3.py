3d6b72792b8570c75da58a39f6f9bb554597001d
tududundra <sashamattini@mail.ru>
2016-12-06 16:08:01 +0300

    Сюжет: "В Сети появилось видео визита Грефа в Сбербанк под видом инвалида"

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -22,7 +22,6 @@ def reader():
     text_arr = []
     for html in d:
         reg2 = re.findall('<p>(.+?)</p>', html)
-        print(reg2)
         if reg2 != []:
             text = ' '.join(reg2)
             text_arr.append(text)
@@ -30,28 +29,24 @@ def reader():
 
 
 def cleaner(text_arr):
+    clean_arr = []
     for el in text_arr:
-        '<.+?>'.sub(' ', el)
-        el.replace('\xa0', ' ')
+        el = re.sub('<.*?>', ' ', el)
+        el = re.sub('&.+?;', ' ', el)
+        el = re.sub('[/()\.\?,\$!–\"«»№:—=1-90]', '', el)
+        el = re.sub(' +', ' ', el)
+        el = re.sub('\xa0', ' ', el)
         html.unescape(el)
-    return text_arr
+        clean_arr.append(el)
+    return clean_arr
 
 
-#def arr_maker():
-#    text_arr = []
-#    while len(text_arr) != 5 :
-#        clean_text = cleaner ()
-#        text_arr.append(clean_text)
-#    return text_arr
 
 
 def counter(text_arr):
-    f = open('links', 'r', encoding='UTF-8')
-    urls = f.readlines()
-    f.close()
+
     arr1 = text_arr[0].split(' ')
     set1 = set(arr1)
-    print(set1)
     arr2 = text_arr[1].split(' ')
     set2 = set(arr2)
     arr3 = text_arr[2].split(' ')
@@ -63,35 +58,23 @@ def counter(text_arr):
     unique2 = set2 - set1 - set3 - set4
     unique3 = set3 - set1 - set2 - set4
     unique4 = set4 - set1 - set2 - set3
-    f_crossing = open('crossing.txt', 'a', encoding='UTF-8')
+    f_crossing = open('crossing.txt', 'w', encoding='UTF-8') #файл с общими словами
     for el in sorted(crossing):
         f_crossing.write(el + '\n')
     f_crossing.close()
-    f_unique = open('unique.txt', 'a', encoding='UTF-8')
-    f_unique.write(urls[0])
-    for el1 in sorted(unique1):
-        a = text_arr[0].split(' ')
-        el1 = el1.strip(',.?<>():;')
-        if a.count(el1) > 1:
-            f_unique.write(el1 + '\n')
-    f_unique.write(urls[1])
-    for el2 in sorted(unique2):
-        a = text_arr[1].split(' ')
-        el2 = el2.strip(',.?<>():;')
-        if a.count(el2) > 1:
-            f_unique.write(el2 + '\n')
-    f_unique.write(urls[2])
-    for el3 in sorted(unique3):
-        a = text_arr[2].split(' ')
-        el3 = el3.strip(',.?<>():;')
-        if a.count(el3) > 1:
-            f_unique.write(el3 + '\n')
-    f_unique.write(urls[3])
-    for el4 in sorted(unique4):
-        a = text_arr[3].split(' ')
-        el4 = el4.strip(',.?<>():;')
-        if a.count(el4) > 1:
-            f_unique.write(el4 + '\n')
+    f_unique = open('unique.txt', 'w', encoding='UTF-8') #файл с уникальными словами
+    unique = unique1 | unique2 | unique3 | unique4
+    for w in unique:
+        if ' ' in w:
+            un = set(w)
+            unique = un | unique
+    for word in sorted(unique):
+        text = ' '.join(text_arr)
+        a = text.split(' ')
+        word = word.strip(' ,.?<>():;\"«»=')
+        if a.count(word) > 1:
+            f_unique.write(word + '\n')
+
 
 
 
